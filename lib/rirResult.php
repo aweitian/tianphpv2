e82c5e3de07db9c3358bb7f773dd8cf5bcf81d11
awei.tian <awei.tian@qq.com>
2016-04-13 18:32:37 +0800

    rirResult is added

---
+++ lib/rirResult.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Date: Apr 13, 2016
+ * Author: Awei.tian
+ * Description: 
+ */
+class rirResult{
+	public $result = 0;
+	public $info;
+	public $return;
+	public function __construct($result=0,$info="",$return=array()){
+		$this->result = $result;
+		$this->info = $info;
+		$this->return = $return;
+	}
+	public function isTrue(){
+		return $this->result == 0;
+	}
+}
