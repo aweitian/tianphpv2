73d49207716918be373223084e112eb0e1f96cd1
awei.tian <awei.tian@qq.com>
2016-07-23 08:46:53 +0000

    db

--- app/runEnvir/default.php
+++ app/runEnvir/default.php
@@ -8,5 +8,5 @@ define("DB_HOST","localhost");
 define("DB_PORT",3306);
 define("DB_USER","root");
 define("DB_PASS","root");
-define("DB_NAME","tianv2");
+define("DB_NAME","fx120120nanzicom");
 define("DB_CHARSET","utf8");
