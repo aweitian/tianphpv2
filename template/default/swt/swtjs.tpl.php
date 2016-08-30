e6bb355911cdec1a8c9f785a89c1af31b99af5da
awei.tian <awei.tian@qq.com>
2016-08-30 19:56:26 +0800

    m swt

--- template/default/swt/swtjs.tpl.php
+++ template/default/swt/swtjs.tpl.php
@@ -26,15 +26,7 @@ function isDoctor($msg)
 		return false;
 }
 ?>
-<!--
 
-var LiveAutoInvite0='您好，来自%IP%的朋友';
-var LiveAutoInvite1='来自首页自动邀请的对话';
-var LiveAutoInvite2='<iframe scrolling=no height=245 frameborder=0 width=400 align=center marginwidth=0 src=http://www.long120.com/tc.html charset="utf-8" marginheight=0>';
-var LiveAutoInvite2='<P align=center><IMG src=\\"http://www.long120.com/images/kefu4.gif\\" border=0></P>';
-var LR_next_invite_seconds = 15;    //10秒后再次显示自动邀请
-var LrinviteTimeout = 1;     //8秒后第一次自动弹出
-//-->
 document.writeln("<script language=\"javascript\" src=\"<?php print AppChannel::getSwt()?>\"></script>");
 
 document.writeln("<style type=\"text/css\">#LRfloater0{display:none;}#LRfloater1{display:none;}</style>");
--- template/default_m/bottomjs.tpl.php
+++ template/default_m/bottomjs.tpl.php
@@ -4,6 +4,9 @@
 
 <div class="backToTop" style="display: block;"></div>
 
+
+
+<script type="text/javascript" src="<?php print AppUrl::swtjs()?>?m=<?php print AppModule::$moduleName?><?php if(AppModule::$moduleName == "doctor")print '&n='.$this->model->data["id"]?><?php /*if(AppModule::$moduleName == "default" && appCtrl::$msg->getControl()=="main")print '&c=m'*/?>"></script>
 <script src="<?php print AppUrl::getMediaPath()?>/js/touch_new_index.js" type="text/javascript" charset="utf-8"></script>
 <script src="<?php print AppUrl::getMediaPath()?>/js/swt.js" type="text/javascript" charset="utf-8"></script>
 
